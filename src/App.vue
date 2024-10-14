e983551b0d9f393234792f7507afbef999390103
xodnd0507 <24457545yong@gmail.com>
2024-10-14 16:39:59 +0900

    test : Test click event

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,22 @@
     <nav>
       <a href="#" v-for="(a,i) in menu" :key="i">{{ a }}</a>
     </nav>
-    <div v-for="(p,i) in products" :key="i">
+    <!-- <div v-for="(p,i) in products" :key="i">
       <h4>{{ p }}</h4>
-      <p>{{ price[i] }} 만원</p>
+      <p>{{ price[i] }}만원</p>
+    </div> -->
+    <div>
+      <h4>{{ products[0] }}</h4>
+      <p>50만원</p>
+      <button @click="num++">허위매물신고</button> <span>신고수 : {{num}}</span>
+    </div>
+    <div>
+      <h4>{{ products[1] }}</h4>
+      <p>60만원</p>
+    </div>
+    <div>
+      <h4>{{ products[2] }}</h4>
+      <p>70만원</p>
     </div>
   </div>
 </template>
@@ -15,6 +28,7 @@ export default {
   name: 'App',
   data(){
     return{
+      num : 0,
       menu :['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
       price : ['50', '60', '70'],
