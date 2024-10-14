2d8bbd2279b442a78d176e662abdf3962fbed841
xodnd0507 <24457545yong@gmail.com>
2024-10-14 14:43:41 +0900

    test : Update

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,16 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <div>
-      <h4>XX 원룸</h4>
-      <p>{{ price1 }} 만원</p>
+      <h4>{{ products[0] }}</h4>
+      <p>50 만원</p>
     </div>
     <div>
-      <h4>XX 원룸</h4>
-      <p>XX 만원</p>
+      <h4>{{ products[1] }}</h4>
+      <p>60 만원</p>
+    </div>
+    <div>
+      <h4>{{ products[2] }}</h4>
+      <p>70 만원</p>
     </div>
   </div>
 </template>
@@ -18,8 +22,7 @@ export default {
   name: 'App',
   data(){
     return{
-      price1 : "60",
-
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸']
     }
   },
   components: {
